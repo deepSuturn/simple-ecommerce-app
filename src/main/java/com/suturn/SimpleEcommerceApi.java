9c2d3263db430c02977ddf750cb7036a96e9627a
Suturn <jean.adrianus@gmail.com>
2025-01-07 23:26:34 -0300

    adding spring-boot tags

--- src/main/java/com/suturn/SimpleEcommerceApi.java
+++ src/main/java/com/suturn/SimpleEcommerceApi.java
@@ -1,5 +1,8 @@
 package com.suturn;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SimpleEcommerceApi {
     public static void main(String[] args) {
         System.out.println("Hello World!");
